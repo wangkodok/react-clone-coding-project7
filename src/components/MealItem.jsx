5ac6cda2e38243bc1559f9e3885f605ee2475eaa
wangkodok <wangkodok@gmail.com>
2024-06-03 17:26:28 +0900

    utils 폴더에서 코드 사용

--- src/components/MealItem.jsx
+++ src/components/MealItem.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { currencyFormatter } from "../utils/formatting.js";
 
 export default function MealItem({ meal }) {
   return (
@@ -7,7 +8,9 @@ export default function MealItem({ meal }) {
         <img src={`http://localhost:3000/${meal.image}`} alt={meal.name} />
         <div>
           <h3>{meal.name}</h3>
-          <p className="meal-item-price">{meal.price}</p>
+          <p className="meal-item-price">
+            {currencyFormatter.format(meal.price)}
+          </p>
           <p className="meal-item-description">{meal.description}</p>
         </div>
         <p className="meal-item-actions">
