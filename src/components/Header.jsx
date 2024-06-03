4e5b1d4bbe34c76587e24a129ea530c29881aff3
wangkodok <wangkodok@gmail.com>
2024-06-03 17:45:35 +0900

    버튼 재사용 컴포넌트 생성

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import logoImg from "../assets/logo.jpg";
+import Button from "./UI/Button";
 
 export default function Header() {
   return (
@@ -8,7 +9,7 @@ export default function Header() {
         <h1>react food</h1>
       </div>
       <nav>
-        <button>cart 0</button>
+        <Button textOnly={true}>cart 0</Button>
       </nav>
     </header>
   );
