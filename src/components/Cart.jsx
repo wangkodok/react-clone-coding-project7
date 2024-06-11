ae33687e5747355aeb49ac0764e2527c0d3849f9
wangkodok <wangkodok@gmail.com>
2024-06-11 16:55:25 +0900

    장바구니 수량 버튼

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -5,6 +5,7 @@ import CartContext from "../store/CartContext";
 import Button from "./UI/Button";
 import { currencyFormatter } from "../utils/formatting";
 import UserProgressContext from "../store/UserProgressContext.jsx";
+import CartItem from "./CartItem.jsx";
 
 export default function Cart() {
   const cartCtx = useContext(CartContext);
@@ -24,9 +25,15 @@ export default function Cart() {
       <h2>장바구니</h2>
       <ul>
         {cartCtx.items.map((item) => (
-          <li key={item.id}>
-            {item.name} - {item.quantity}
-          </li>
+          // <CartItem key={item.id} {...item} />
+          <CartItem
+            key={item.id}
+            name={item.name}
+            quantity={item.quantity}
+            price={item.price}
+            onIncrease={() => cartCtx.addItem(item)}
+            onDecrease={() => cartCtx.removeItem(item.id)}
+          />
         ))}
       </ul>
       <p className="cart-total">{currencyFormatter.format(cartTotal)}</p>
--- src/store/CartContext.jsx
+++ src/store/CartContext.jsx
@@ -30,7 +30,7 @@ function cartReducer(state, action) {
 
   if (action.type === "REMOVE_ITEM") {
     const existingCartItemIndex = state.items.findIndex(
-      (item) => item.id === action.item.id
+      (item) => item.id === action.id
     );
     const existingCartItem = state.items[existingCartItemIndex];
 
